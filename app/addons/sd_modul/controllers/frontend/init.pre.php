e335686dc84b2d19424e98f63d4931908c325373
Nail Khabibullin <cznailmail@gmail.com>
2022-06-19 19:32:52 +0400

    addon.xml, init.pre.php, func.php

---
+++ app/addons/sd_modul/controllers/frontend/init.pre.php
@@ -0,0 +1,39 @@
+<?php
+/***************************************************************************
+*                                                                          *
+*   (c) 2004 Vladimir V. Kalynyak, Alexey V. Vinokurov, Ilya M. Shalnev    *
+*                                                                          *
+* This  is  commercial  software,  only  users  who have purchased a valid *
+* license  and  accept  to the terms of the  License Agreement can install *
+* and use this program.                                                    *
+*                                                                          *
+****************************************************************************
+* PLEASE READ THE FULL TEXT  OF THE SOFTWARE  LICENSE   AGREEMENT  IN  THE *
+* "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
+****************************************************************************/
+
+use Tygh\Registry;
+
+defined('BOOTSTRAP') or die('Access denied');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $suffix = '';
+    if (!isset($_COOKIE['approve'])) {
+        $suffix = '.modul';
+    } elseif(isset($_COOKIE['approve']) && $_COOKIE['approve'] == 0) {
+        $suffix = '.denied';
+    }
+}
+
+if ($mode === 'modul') {
+    if (!empty($_POST)) {
+        $age = fn_age_calculation($birthday);
+            if ($age >=18) {
+                SetCookie('approve', 1);
+                fn_print_die ($_COOKIE);
+                } else {
+                    SetCookie('approve', 0);
+                    $suffix = '.denied';
+                }
+        }
+}
---
+++ app/addons/sd_modul/func.php
@@ -0,0 +1,24 @@
+<?php
+
+use Tygh\Languages\Languages;
+
+defined('BOOTSTRAP') or die('Access denied')
+
+function fn_age_calculation($birthday) {
+    if (!empty($_POST['guest_data']['birthday'])) {
+        $birthday = $_POST['guest_data']['birthday'];
+        $year = date('Y', $birthday);
+        $month = date('m-d', $birthday);
+
+        $_year = date('Y', TIME);
+        $_month = date('m-d', TIME);
+
+        $age = $_year - $year;
+
+        if ($month > $_month) {
+            $age--;
+        }
+        return $age;
+    }
+    return false;
+}
