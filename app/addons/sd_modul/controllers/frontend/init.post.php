16105a7a0f63ca61ee7cf35227aa75288a2cc35f
Nail Khabibullin <cznailmail@gmail.com>
2022-06-21 14:18:43 +0400

    update func.php & rename init.pre.php -> init.post.php

--- app/addons/sd_modul/controllers/frontend/init.post.php
+++ app/addons/sd_modul/controllers/frontend/init.post.php
@@ -15,25 +15,39 @@
 use Tygh\Registry;
 
 defined('BOOTSTRAP') or die('Access denied');
+/*
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {*/
+    //$suffix = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $suffix = '';
-    if (!isset($_COOKIE['approve'])) {
-        $suffix = '.modul';
-    } elseif(isset($_COOKIE['approve']) && $_COOKIE['approve'] == 0) {
-        $suffix = '.denied';
+    if (!isset($_COOKIE['approve']) && !($mode === 'modul')) {
+        return [CONTROLLER_STATUS_OK, 'index.modul'];
+    } elseif (isset($_COOKIE['approve']) && $_COOKIE['approve'] == 0 && !($mode === 'denied')) {
+        return [CONTROLLER_STATUS_OK, 'index.denied'];
     }
-}
+//}
 
 if ($mode === 'modul') {
+
     if (!empty($_POST)) {
         $age = fn_age_calculation($birthday);
+        //fn_print_die ($age);
             if ($age >=18) {
                 SetCookie('approve', 1);
                 //fn_print_die ($_COOKIE);
+                return [CONTROLLER_STATUS_REDIRECT, 'index.index'];
                 } else {
                     SetCookie('approve', 0);
-                    $suffix = '.denied';
+                    //$suffix = '.denied';
+                    //fn_print_die ($_COOKIE);
+                    return [CONTROLLER_STATUS_REDIRECT, 'index.denied'];
                 }
         }
 }
+
+/*if (!($mode === 'modul')) {
+
+}
+
+if (!($mode === 'denied')) {
+
+}*/
--- app/addons/sd_modul/func.php
+++ app/addons/sd_modul/func.php
@@ -7,8 +7,8 @@ defined('BOOTSTRAP') or die('Access denied');
 function fn_age_calculation($birthday) {
     if (!empty($_POST['guest_data']['birthday'])) {
         $birthday = $_POST['guest_data']['birthday'];
-        $year = date('Y', $birthday);
-        $month = date('m-d', $birthday);
+        $year = date('Y', strtotime($birthday));
+        $month = date('m-d', strtotime($birthday));
 
         $_year = date('Y', TIME);
         $_month = date('m-d', TIME);
